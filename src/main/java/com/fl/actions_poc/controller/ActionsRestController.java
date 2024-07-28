b93efa81657ffad236cf91acad27ac9bee0baebb
Fran Larrocca <francisco.larrocca@outlook.com>
2024-07-28 03:58:38 -0300

    added test cases and jacoco report

--- src/main/java/com/fl/actions_poc/controller/ActionsRestController.java
+++ src/main/java/com/fl/actions_poc/controller/ActionsRestController.java
@@ -10,14 +10,22 @@ public class ActionsRestController {
 
     @GetMapping("/add")
     public Map<String, Integer> add(@RequestParam int a, @RequestParam int b) {
+        validateArguments(a, b);
         int result = a + b;
         return Map.of("result", result);
     }
 
     @GetMapping("/multiply")
     public Map<String, Integer> multiply(@RequestParam int a, @RequestParam int b) {
+        validateArguments(a, b);
         int result = a * b;
         return Map.of("result", result);
+    }
+
+    private void validateArguments(int a, int b) {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Both arguments 'a' and 'b' must be positive");
+        }
     }
 
 
